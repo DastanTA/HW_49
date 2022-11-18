14679ce6de265ba40d7595ceed35ff6139eb946e
Dastan T <dastan.turatbekov@gmail.com>
2022-11-18 13:00:24 +0600

    Этап 2. Вывод главной страницы - реализован

--- source/task_tracker/views.py
+++ source/task_tracker/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from task_tracker.models import Task
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+
+class MainPage(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        print(context)
+        return context
