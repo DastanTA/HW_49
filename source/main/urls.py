7e9d40c097cd635c80e3dff884edbb0ad5e922ab
Dastan T <dastan.turatbekov@gmail.com>
2022-11-18 13:20:10 +0600

    Этап 2. Детальный просмотр задачи - реализован

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from task_tracker.views import MainPage
+from task_tracker.views import MainPage, TaskView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPage.as_view(), name='main'),
+    path('task/<int:pk>', TaskView.as_view(), name='view_task'),
 ]
--- source/task_tracker/views.py
+++ source/task_tracker/views.py
@@ -11,3 +11,12 @@ class MainPage(TemplateView):
         context['tasks'] = Task.objects.all()
         print(context)
         return context
+
+
+class TaskView(TemplateView):
+    template_name = 'task.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+        return context
