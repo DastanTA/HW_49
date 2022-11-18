a7f2eeef0af45488ccfb5eb7f33f7ddf0842af77
Dastan T <dastan.turatbekov@gmail.com>
2022-11-18 14:01:06 +0600

    Этап 2. Создание новой задачи - реализовано

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from task_tracker.views import MainPage, TaskView
+from task_tracker.views import MainPage, TaskView, CreateTask
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPage.as_view(), name='main'),
     path('task/<int:pk>', TaskView.as_view(), name='view_task'),
+    path('add/', CreateTask.as_view(), name='add_task'),
 ]
--- source/task_tracker/views.py
+++ source/task_tracker/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from task_tracker.models import Task
 from django.views.generic import View, TemplateView
+from task_tracker.forms import TaskForm
 
 
 class MainPage(TemplateView):
@@ -20,3 +21,23 @@ class TaskView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
         return context
+
+
+class CreateTask(View):
+    def get(self, request, *args, **kwargs):
+        form = TaskForm()
+        context = {'form': form}
+        return render(request, 'create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            Task.objects.create(
+                summary=form.cleaned_data.get('summary'),
+                description=form.cleaned_data.get('description'),
+                status=form.cleaned_data.get('status'),
+                type=form.cleaned_data.get('type')
+            )
+            return redirect('main')
+        else:
+            return render(request, 'create.html', {'form': form})
